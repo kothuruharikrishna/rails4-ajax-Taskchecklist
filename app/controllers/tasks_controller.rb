01c77f1c74e08e24cf2cfcac1967ac320f6f06ee
hari krishna <youremail@example.com>
2015-01-03 15:46:09 +0530

    checkbox ajax example

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,35 @@
+class TasksController < ApplicationController
+	def index
+    @incomplete_tasks = Task.where(complete: false)
+    @complete_tasks = Task.where(complete: true)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create!(params.require(:task).permit(:name))
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes!(params.require(:task).permit(:complete))
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
+  end
+
+  def destroy
+    @task = Task.destroy(params[:id])
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
+  end
+end
